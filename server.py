d76e1988199d1ade981238290350dcbf7b081b72
Thifnmi <tuthin2k@gmail.com>
2021-11-29 15:18:21 +0700

    aaaaaa

--- server.py
+++ server.py
@@ -2,11 +2,11 @@ from app import run_server, run_smtp
 from multiprocessing import Process
 
 
-def run():
-    p1 = Process(target=run_server, args=(8080, ))
+def run(port1, port2):
+    p1 = Process(target=run_server, args=(port1, ))
     p1.daemon = True
     p1.start()
-    p2 = Process(target=run_smtp, args=(1025, ))
+    p2 = Process(target=run_smtp, args=(port2, ))
     p2.daemon = True
     p2.start()
     p1.join()
@@ -14,4 +14,4 @@ def run():
 
 
 if __name__ == "__main__":
-    run()
+    run(8080, 1025)
